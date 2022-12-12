cb0a295dbba9535c545a74d9f035fec2a4637efd
GuiFxFarias <109701085+GuiFxFarias@users.noreply.github.com>
2022-12-12 20:18:06 -0300

    feature: starting coding to the list

--- scripts/javascript/index.js
+++ scripts/javascript/index.js
@@ -1,6 +1,7 @@
 "use strict";
 const page = document.querySelector("body");
 if (page) {
+    // Criando funcionalidade do 'menu-mobile'
     const btnOC = page.querySelector(".btn-OC");
     const menuBar = page.querySelector(".itensClosed");
     const itensBought = page.querySelector(".boughtItens");
@@ -15,4 +16,21 @@ if (page) {
         menuBar.classList.toggle("close");
         itensBought.classList.toggle("open");
     });
+    // Encerrado 'menu-mobile'. Stts: !! Funcionando !!
+    // Adicionando Itens no Carrinho
+    const btnImage = page.querySelectorAll(".btn-image");
+    const item = page.querySelectorAll(".item");
+    const imageItem = page.querySelectorAll('.listItens img');
+    let listItens = [];
+    btnImage.forEach((buttons) => {
+        const buttonsImage = buttons.querySelector("li");
+        buttonsImage.addEventListener("click", (e) => {
+            e.preventDefault();
+            const element = e.target;
+            if (element.value) {
+                listItens.push(element.value);
+            }
+        });
+    });
+    // Em progresso 'carrinho'. Stts: !! Em construção !!
 }
--- scripts/typescript/index.ts
+++ scripts/typescript/index.ts
@@ -1,25 +1,53 @@
-const page = document.querySelector("body")
+const page = document.querySelector("body");
 
-if (page){
+if (page) {
+  // Criando funcionalidade do 'menu-mobile'
+  const btnOC = page.querySelector(".btn-OC") as HTMLButtonElement;
+  const menuBar = page.querySelector(".itensClosed") as HTMLDivElement;
+  const itensBought = page.querySelector(".boughtItens") as HTMLDivElement;
+  const btn = page.querySelector(".btn") as HTMLButtonElement;
 
-    const btnOC = page.querySelector(".btn-OC") as HTMLButtonElement
-    const menuBar = page.querySelector(".itensClosed") as HTMLDivElement
-    const itensBought = page.querySelector(".boughtItens") as HTMLDivElement
-    const btn = page.querySelector(".btn") as HTMLButtonElement
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
 
-    btn.addEventListener("click", (e) =>{
-        e.preventDefault()
+    menuBar.classList.toggle("close");
+    itensBought.classList.toggle("open");
+  });
 
-        menuBar.classList.toggle("close")
-        itensBought.classList.toggle("open")
-    })
+  btnOC.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    menuBar.classList.toggle("close");
+    itensBought.classList.toggle("open");
+  });
+  // Encerrado 'menu-mobile'. Stts: !! Funcionando !!
+
+  // Adicionando Itens no Carrinho
+  const btnImage = page.querySelectorAll(
+    ".btn-image"
+  ) as NodeListOf<HTMLElement>;
+  const item = page.querySelectorAll(".item") as NodeListOf<HTMLElement>
+  const imageItem = page.querySelectorAll('.listItens img') as NodeListOf<HTMLElement>
+
+  let listItens = [] as number[]
 
 
-    btnOC.addEventListener("click", (e) =>{
-        e.preventDefault()
+  btnImage.forEach((buttons) => {
+    const buttonsImage = buttons.querySelector("li") as HTMLLIElement;
 
-        menuBar.classList.toggle("close")
-        itensBought.classList.toggle("open")
-    })
+    buttonsImage.addEventListener("click", (e) => {
+      e.preventDefault()
+    
+      const element = e.target as HTMLLIElement
+      
+      if(element.value){
+        listItens.push(element.value)
+      }
+    });
+  });
 
+
+  
+
+  // Em progresso 'carrinho'. Stts: !! Em construção !!
 }
